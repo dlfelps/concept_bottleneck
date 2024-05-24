c03f83d5cab6a70db691c1a40fc2b06ee8962bf0
Daniel Felps <dlfelps@gmail.com>
2024-05-24 15:02:56 -0400

    predict_embeddings

---
+++ utils.py
@@ -0,0 +1,44 @@
+from typing import List, Optional, Tuple
+
+import numpy as np
+import pandas as pd
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
+
+def predict_embeddings(
+    dataloader: DataLoader,
+    model: nn.Module,
+    device: Optional[str] = None,
+) -> pd.DataFrame:
+    """
+    Predict embeddings for a dataloader.
+    Args:
+        dataloader: dataloader to predict embeddings for. Must deliver tuples (images, class_names)
+        model: model to use for prediction
+        device: device to cast the images to. If none, no casting is performed. Must be the same as
+            the device the model is on.
+    Returns:
+        dataframe with columns embedding and class_name
+    """
+    all_embeddings = []
+    all_class_names = []
+
+    model.eval()
+    with torch.no_grad():
+        for images, class_names in tqdm(
+            dataloader, unit="batch", desc="Predicting embeddings"
+        ):
+            if device is not None:
+                images = images.to(device)
+            all_embeddings.append(model(images).detach().cpu())
+            if isinstance(class_names, torch.Tensor):
+                all_class_names += class_names.tolist()
+            else:
+                all_class_names += class_names
+
+    concatenated_embeddings = torch.cat(all_embeddings)
+
+    return list(concatenated_embeddings),  all_class_names
